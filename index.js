ab320216c7b172bf5b95da95aac44d7fae200492
Adriana <adriana44@free.fr>
2024-10-25 22:07:15 +0200

    creer des routes

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const data = require("./posts");
+const PORT = process.env.PORT || 5000;
+
+let posts = [];
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/posts', (req, res) => {
+    const data_posts = [...posts, ...data];
+    res.send(data_posts);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+});
