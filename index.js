8d622a77e0c934e4ca256657114edf3b9fc2d420
Adriana <adriana44@free.fr>
2024-10-28 11:42:05 +0100

    method create post added

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const app = express();
 const data = require("./posts");
 const PORT = process.env.PORT || 5000;
 
+
 let posts = [];
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+  
 app.get('/posts', (req, res) => {
     const data_posts = [...posts, ...data];
     res.send(data_posts);
   });
+
+  app.get('/posts/create', (req, res) => {
+    const data_posts = [...posts, ...data, req.body];
+    res.send('new post successfully created');
+  });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
