832d8ff10e4de4e68816e600b1f77212c3131490
Adriana <adriana44@free.fr>
2024-11-05 17:28:00 +0100

    conection to mongodb successfully yeahhh

--- models.js
+++ models.js
@@ -2,14 +2,15 @@ const mongoose = require("mongoose");
 
 const PostSchema =  mongoose.Schema({
     title: {type: String , required: true },
-    Conten: { type: String, required: true },
+    content: { type: String, required: true },
     created_at: { type: Date, default: Date.now },
     author:{type: String, required: true},
    
-})
+});
 
 PostSchema.methods.toLocalString = function() {
-    
+// Define a method to convert the created_at date   
     return this.created_at.toLocaleString();
-}
+};
+// Export the Post model
 module.exports = mongoose.model("Post", PostSchema);
